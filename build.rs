09425927b7dcda4450a4a7f4cef6d455e527a512
cavalleroyt <cavalleroyt@gmail.com>
2026-03-07 14:46:21 +0100

    Add files via upload

--- build.rs
+++ build.rs
@@ -1,8 +1,10 @@
 fn main() {
     // run asset synchronization if possible
+    let script_path = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("scripts/sync_assets.sh");
+    
     if std::process::Command::new("sh")
-        .arg("-c")
-        .arg("$(pwd)/scripts/sync_assets.sh")
+        .arg(script_path)
         .status()
         .is_ok()
     {
